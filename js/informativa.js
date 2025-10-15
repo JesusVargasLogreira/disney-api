1b467e8e5fabac7fd910f681e8bbeda0a45dc101
Jesus <jesusmariavargaslogreira@gmail.com>
2025-10-15 17:45:33 -0500

    sexto commit

---
+++ js/informativa.js
@@ -0,0 +1,10 @@
+function Informativa() {
+    var root = document.getElementById("root");
+    root.innerHTML = `
+    <h1>DisneyApi</h1>
+    <h2>Jesus Maria Vargas Logreira</h2>
+    <img style="width: 150px; height: auto;" src="mickey-mouse.png" alt="disneyapi">
+    <p>https://github.com/JesusVargasLogreira/disney-api</p>
+    <p>v 1.0</p>    
+    `
+}
